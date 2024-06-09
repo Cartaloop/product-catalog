084164078dd55afe66551f94ed11c24c5d85bacb
Cartaloop <lucasrech00@gmail.com>
2024-06-09 18:51:26 -0300

    Initial commit

---
+++ src/test/java/edu/lucasrech/product_catalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lucasrech.product_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
